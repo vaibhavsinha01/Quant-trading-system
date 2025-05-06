447179e075248f11b0c17e89f7d5c2055dc72889
Vaibhav <vaibhavrajsinha099@gmail.com>
2025-05-06 11:06:17 +0530

    lstm done autobnn left

--- strategies/creds.py
+++ strategies/creds.py
@@ -7,7 +7,7 @@ token = 'AKHRYAVFX36WALYNW2YIJ27ADI'
 """ information about the stocks that would be traded """
 equity_choice = 0 # changed from 0 
 exchange = "NSE" # - used 
-token_id = "3045" # - used # "99926000" # "2885" # "3045" # token_id = "11536" # token_id = "99926000" # token_id="12815"# token_id="9931"
-tradingsymbol = "SBIN-EQ" # - used # "NIFTY50" # "SBIN-EQ" # changed from SBIN-EQ # tradingsymbol = "SBIN-EQ" # tradingsymbol = "TCS-EQ" # tradingsymbol = "NIFTY50" #tradingsymbol="TATAINVEST-BL" # tradingsymbol="VIKASLIFE-EQ"
+token_id = "2885" # - used # "99926000" # "2885" # "3045" # token_id = "11536" # token_id = "99926000" # token_id="12815"# token_id="9931"
+tradingsymbol = "RELIANCE-EQ" # - used # "NIFTY50" # "SBIN-EQ" # changed from SBIN-EQ # tradingsymbol = "SBIN-EQ" # tradingsymbol = "TCS-EQ" # tradingsymbol = "NIFTY50" #tradingsymbol="TATAINVEST-BL" # tradingsymbol="VIKASLIFE-EQ"
 timeframe = "ONE_DAY"
-use_data_for_x_days = 3000
+use_data_for_x_days = 2000
